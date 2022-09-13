cd5e2fad67a13aa34f6e83c7d7072f55644b9889
Jin Mok <102392728+mokkang@users.noreply.github.com>
2022-09-12 18:57:23 -0700

    Update promote.sh

--- promote.sh
+++ promote.sh
@@ -3,20 +3,20 @@ echo "			Usage: ./S2P.sh <Veracode Application Profile>  	ex: ./S2P.sh VeraDemo 
 echo "--------------------------------------------------------------------------------------------------------------------------------------------"
 export $1
 echo "Pull down and parse out the latest Sandbox scan through AppID to build_Id"
-sappid=$(java -jar /Users/jmok/VeracodeJavaAPI.jar -action GetAppBuilds | grep $1 | tail -n 2 | head -n 1 |awk '{print $2}' |sed 's/[^0-9]*//g')
+appid=$(java -jar VeracodeJavaAPI.jar -action GetAppBuilds | grep $1 | tail -n 2 | head -n 1 |awk '{print $2}' |sed 's/[^0-9]*//g')
 set | grep sappid
 printenv | grep sappid
-echo $sappid
+echo $appid
 echo "Get Sandboxid"
-sandboxID=$(java -jar /Users/jmok/VeracodeJavaAPI.jar -action GetSandboxList -appid=$sappid -vid="$VID" -vkey="$VKEY" | grep sandbox_id | awk '{print $6}' | tail -n 1 | sed 's/[^0-9]*//g')
+sandboxID=$(java -jar VeracodeJavaAPI.jar -action GetSandboxList -appid=$appid -vid="$(VERACODE_API_ID)" -vkey="$(VERACODE_API_KEY)" | grep sandbox_id | awk '{print $6}' | tail -n 1 | sed 's/[^0-9]*//g')
 echo $sandboxID | awk '{print $1}'
 set | grep sandboxID
 printenv | grep sandboxID
 echo $sandboxID
-buildID=$(java -jar /Users/jmok/VeracodeJavaAPI.jar -action GetBuildList -appid $sappid -sandboxid $sandboxID | grep build_id | awk '{print $2}' | tail -n 1 | sed 's/[^0-9]*//g')
+buildID=$(java -jar VeracodeJavaAPI.jar -action GetBuildList -appid $appid -sandboxid $sandboxID | grep build_id | awk '{print $2}' | tail -n 1 | sed 's/[^0-9]*//g')
 echo $buildID
 set | grep buildID
 printenv | grep buildID
 echo "Prep for Promotion"
-java -jar /Users/jmok/VeracodeJavaAPI.jar -action PromoteSandbox -buildid $buildID
+java -jar VeracodeJavaAPI.jar -action PromoteSandbox -buildid $buildID
 echo "Promoted"
