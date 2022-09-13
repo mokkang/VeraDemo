65895070b3f3d70e27bc2b460db5c272fc38a339
Jin Mok <102392728+mokkang@users.noreply.github.com>
2022-09-13 09:41:39 -0700

    Update p2s.sh

--- p2s.sh
+++ p2s.sh
@@ -3,10 +3,10 @@ echo "			Usage: ./S2P.sh <Veracode Application Profile>  	ex: ./S2P.sh VeraDemo 
 echo "--------------------------------------------------------------------------------------------------------------------------------------------"
 echo "Pull down and parse out the latest Sandbox scan through AppID to build_Id"
 echo $1
-#export $1
+export $1
 curl -sO https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/22.6.10.2/vosp-api-wrappers-java-22.6.10.2.jar
 chmod +x vosp-api-wrappers-java-22.6.10.2.jar
-appid=$(java -jar vosp-api-wrappers-java-22.6.10.2.jar -action GetAppBuilds -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY | grep S2P | tail -n 2 | head -n 1 |awk '{print $2}' |sed 's/[^0-9]*//g')
+appid=$(java -jar vosp-api-wrappers-java-22.6.10.2.jar -action GetAppBuilds -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY | grep $1 | tail -n 2 | head -n 1 |awk '{print $2}' |sed 's/[^0-9]*//g')
 set | grep sappid
 printenv | grep sappid
 echo $appid
