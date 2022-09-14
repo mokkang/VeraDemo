3200fe0848efda33711798ca7a056f8e49fc790a
Jin Mok <102392728+mokkang@users.noreply.github.com>
2022-09-14 07:31:39 -0700

    Update p2s.sh

--- p2s.sh
+++ p2s.sh
@@ -7,8 +7,8 @@ echo "Pull down and parse out the latest Sandbox scan through AppID to build_Id"
 curl -sO https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/22.6.10.2/vosp-api-wrappers-java-22.6.10.2.jar
 chmod +x vosp-api-wrappers-java-22.6.10.2.jar
 appid=$(java -jar vosp-api-wrappers-java-22.6.10.2.jar -action GetAppBuilds -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY | grep S2P | tail -n 2 | head -n 1 |awk '{print $2}' |sed 's/[^0-9]*//g')
-set | grep sappid
-printenv | grep sappid
+set | grep appid
+printenv | grep appid
 echo $appid
 echo "Get Sandboxid"
 sandboxID=$(java -jar vosp-api-wrappers-java-22.6.10.2.jar -action GetSandboxList -appid=$appid -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY | grep sandbox_id | awk '{print $6}' | tail -n 1 | sed 's/[^0-9]*//g')
