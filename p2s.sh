dd3390e939d38bf4a4168ed8d8a67fd6a527e78f
Jin Mok <102392728+mokkang@users.noreply.github.com>
2022-09-15 22:39:04 -0700

    Update p2s.sh

--- p2s.sh
+++ p2s.sh
@@ -16,7 +16,7 @@ echo $sandboxID | awk '{print $1}'
 set | grep sandboxID
 printenv | grep sandboxID
 echo $sandboxID
-buildID=$(java -jar vosp-api-wrappers-java-22.6.10.2.jar -action GetBuildList -appid $appid -sandboxid $sandboxID -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY | grep build_id | awk '{print $2}' | tail -n 1 | sed 's/[^0-9]*//g')
+buildID=$(java -jar vosp-api-wrappers-java-22.6.10.2.jar -action GetBuildList -appid=$appid -sandboxid=$sandboxID -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY | grep build_id | awk '{print $2}' | tail -n 1 | sed 's/[^0-9]*//g')
 echo $buildID
 set | grep buildID
 printenv | grep buildID
