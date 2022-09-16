dc995bbda6e579821a8511a52c72e77db6504284
Jin Mok <102392728+mokkang@users.noreply.github.com>
2022-09-15 23:45:12 -0700

    Update sbox.sh

--- sbox.sh
+++ sbox.sh
@@ -28,5 +28,5 @@ echo $buildID
 set | grep buildID
 @printenv | grep buildID
 echo "Prep for Promotion"
-java -jar vosp-api-wrappers-java-22.6.10.2.jar -action PromoteSandbox -buildid="$buildID" -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY
+java -jar vosp-api-wrappers-java-22.6.10.2.jar -action PromoteSandbox -buildid=$buildID -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY
 echo "Promoted"
