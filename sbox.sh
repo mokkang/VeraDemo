cda4797203e91d36d60b8048e925c322b18ea773
Jin Mok <102392728+mokkang@users.noreply.github.com>
2022-09-15 23:02:37 -0700

    Update sbox.sh

--- sbox.sh
+++ sbox.sh
@@ -6,7 +6,7 @@ echo "Pull down and parse out the latest Sandbox scan through AppID to build_Id"
 #export $1
 curl -sO https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/22.6.10.2/vosp-api-wrappers-java-22.6.10.2.jar
 chmod +x vosp-api-wrappers-java-22.6.10.2.jar
-appid=$(java -jar vosp-api-wrappers-java-22.6.10.2.jar -action GetAppBuilds -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY | grep VeraDemo | tail -n 2 | head -n 1 |awk '{print $2}' |sed 's/[^0-9]*//g')
+appid=$(java -jar vosp-api-wrappers-java-22.6.10.2.jar -action GetAppBuilds -vid=$VERACODE_API_ID -vkey=$VERACODE_API_KEY | grep Verademo-Sbox-Policy | tail -n 2 | head -n 1 |awk '{print $2}' |sed 's/[^0-9]*//g')
 set | grep appid
 printenv | grep appid
 echo $appid
